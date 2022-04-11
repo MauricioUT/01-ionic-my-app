7e308a5a5fda29a84cb4aac9760e79349b4f53cd
Mauricio Utrilla <mutrilla@customsoft.biz>
2022-04-10 20:45:13 -0500

    primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor() { }
+
+  ngOnInit(): void {
+    let numero = 10;
+    debugger; // esto solo se usa en desarrollo mas no cuando se pase a  produccion
+    numero = 20;
+    numero = numero + 10;
+    numero = 0;
+    console.log(numero);
+  }
 
 }
